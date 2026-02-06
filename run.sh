9b2431196c34c68fd948dd230a4277905f8ff19b
tlgb <tlgbkakaka@gmail.com>
2026-02-06 17:57:45 +0700

    handle case co \n giua record phien vai

--- run.sh
+++ run.sh
@@ -15,17 +15,29 @@ fi
 
 echo -e "\n1. Sorting by date desc..."
 awk '
+	NR == 1 { print $0; next }
+	/^[0-9]/ {
+		if (saved != "") print saved;
+		saved = $0
+	}
+	!/^[0-9]/ {
+		saved = saved " " $0
+	}
+	END {
+		if (saved != "") print saved
+	}
+' "$INPUT_FILE" | awk '
 	BEGIN { 
 		FPAT = "([^,]*)|(\"([^\"]|\"\")*\")"; 
 		OFS = "\t" 
 	} 
 	NR == 1 { print $0 }
-	NR > 1 {
+	NR > 1 {	
 		split($16, d, "/");
 		epoch = mktime($19 " " d[1] " " d[2] " 00 00 00");
 		print epoch, $0
 	}
-' "$INPUT_FILE" | (read -r header; echo "$header"; sort -t$'\t' -k1,1rn) | cut -f2- > "$OUTPUT_DATE"
+' | (read -r header; echo "$header"; sort -t$'\t' -k1,1rn) | cut -f2- > "$OUTPUT_DATE"
 echo "Exported: $OUTPUT_DATE"
 
 
